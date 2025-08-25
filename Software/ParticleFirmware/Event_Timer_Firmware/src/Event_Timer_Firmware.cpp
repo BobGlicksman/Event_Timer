d04bc60dc349de2c88bec64100cf8a50d2e98bd6
Bob Glicksman <bobg542492@aol.com>
2025-08-25 11:23:56 -0700

    Added Time Display
    This code provides both a basic hardware test function as well as use of the LocalTimeRK library to obtain the local time and display date and time on the LCD.  A #define statement determines which functionality the code will test.

--- Software/ParticleFirmware/Event_Timer_Firmware/src/Event_Timer_Firmware.cpp
+++ Software/ParticleFirmware/Event_Timer_Firmware/src/Event_Timer_Firmware.cpp
@@ -3,17 +3,26 @@
 /*
   Hardware Test for Event Timer
 
-    Basic hardware test using Photon 2 in the RFID Station hardware.
+    Basic hardware and time library software test using Photon 2 in the RFID Station hardware.
 
-    This code displays some stuff on the LCD display and blinks the LEDs 
+    When defined for HARDWARE_TEST, this code displays some stuff on the LCD display and blinks the LEDs 
     in order to demonstrate that the pin mappings are correct.
 
-    Bob Glicksman; 8/24/25
+    Otherwise, this code sets up the local time and displays the current date and time on the LCD.
+
+    (c) 2025 by: Bob Glicksman, Jim Schrempp, Team Practicle Projects; all rights reserved.
+
+    version 1.0.  Initial release: 08-25-2025
 
  */
 
+
 #include <Particle.h>
 #include <LiquidCrystal.h>
+#include <LocalTimeRK.h>
+
+#define VERSION "1.0"
+//#define HARDWARE_TEST   // code to just test the hardware, no time stuff included
 
  // Pinout Definitions for the RFID PCB
  #define ADMIT_LED D19
@@ -27,6 +36,8 @@ LiquidCrystal lcd(D11, D12, D13, D14, D5, D6);
 void flashLED(int ledPin);  // function forward declaration
 
 void setup() {
+  Particle.variable("version", VERSION);  // make the version available to the Console
+
   pinMode(ADMIT_LED, OUTPUT);
   pinMode(REJECT_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
@@ -37,27 +48,54 @@ void setup() {
   digitalWrite(BUZZER, LOW);
   digitalWrite(READY_LED, LOW);
 
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows and clear the display
   lcd.begin(16,2);
-  // Print a message to the LCD.
-  lcd.setCursor(0,0);
-  lcd.print("EVENT TIMER");
+  lcd.clear();
+
+  #ifdef HARDWARE_TEST
+    lcd.setCursor(0,0);
+    lcd.print("EVENT TIMER");
+  #else
+    lcd.setCursor(0,0);
+    // set up the local time (Pacific Time)
+    LocalTime::instance().withConfig(LocalTimePosixTimezone("PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00"));
+  #endif
 
 } // end of setup()
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of 4 second intervals since reset:
-  lcd.print(millis()/4000);
+  #ifdef HARDWARE_TEST
 
-  flashLED(ADMIT_LED);
-  flashLED(REJECT_LED);
-  flashLED(READY_LED);
-// flashLED(BUZZER);   // commented out -- too annoying!
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of 4 second intervals since reset:
+    lcd.print(millis()/4000);
 
-  delay(1000);
+    flashLED(ADMIT_LED);
+    flashLED(REJECT_LED);
+    flashLED(READY_LED);
+    // flashLED(BUZZER);   // commented out -- too annoying!
+
+    delay(1000);
+  #else
+  // display the date and time on the lcd
+    LocalTimeConvert conv;
+    conv.withCurrentTime().convert();
+    
+    String msg;
+    // first line of display is the date
+    msg = conv.format("%Y-%m-%d"); // 2025-08-25
+    lcd.setCursor(0,0);
+    lcd.print(msg);
+
+    // second line of the display is the time
+    msg = conv.format("%I:%M:%S%p"); // 10:00:00AM
+    lcd.setCursor(0,1);
+    lcd.print(msg);
+
+    delay(500);
+  #endif
 
 } // end of loop()
 
